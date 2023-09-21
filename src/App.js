b5568943d1fbb31068d9c75bf5cdce250a8489c1
Akash <kumawat.aakash515@gmail.com>
2023-09-21 15:40:18 +0530

    Project done

--- src/App.js
+++ src/App.js
@@ -1,29 +1,27 @@
-import './App.css';
 import React from 'react';
 import Navbar from './components/Navbar';
-import { BrowserRouter,Routes,Route } from 'react-router-dom'
-
+import './App.css';
+import Home from './components/pages/Home';
+import { BrowserRouter ,Routes,Route } from 'react-router-dom';
+import Services from './components/pages/Services';
+import Products from './components/pages/Products';
+import SignUp from './components/pages/SignUp';
 
 function App() {
   return (
     <>
-     
-     
 <BrowserRouter>
-<Navbar/>
-
-      <Routes>
-     
-      <Route path='/'  />
-          
-       
-    
+        <Navbar />
+        <Routes>
+          <Route path='/' exact component={Home} />
+          <Route path='/services' component={Services} />
+          <Route path='/products' component={Products} />
+          <Route path='/sign-up' component={SignUp} />
+        
       </Routes>
-    </BrowserRouter>
-  </>
-    
+      </BrowserRouter>
+    </>
   );
 }
 
 export default App;
-  
